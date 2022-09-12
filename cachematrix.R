2d70900bc6c527003c35120585612f5bc6e56eff
mikep1233 <mike_perelman@live.com>
2022-09-12 14:05:10 -0500

    saving my copy of the answer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Create the following 2 functions
+# 1) makeCacheMatrix
+# 2) cacheSolve
 
-## Write a short comment describing this function
+#Part 1
+#We will use the mass package to be able to compute the inverse of matrices. 
+#Define the argument, set the inverse to be null & define the function.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+library(MASS)
+makeCacheMatrix <- function(x = numeric()) { 
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  #set the value in our environment so it can be called and build a list. Use ginv to compute the inverse of our matrices.
+  
+  get <- function()x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function(){ 
+    inver <- ginv(x)
+    inver%*%x
+  }
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
+#Part 2: Now we will make a cache that will be able to retrieve this special "matrix" inverse.
 
-## Write a short comment describing this function
+# Define the argument and check if the inverse is NULL.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  #Calculate the inverse values.
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+#Test the function.
+
+test <-makeCacheMatrix(matrix(1:6,2,3))
+test$getinv()
+cacheSolve(test)
